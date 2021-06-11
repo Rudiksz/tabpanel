473d645168f9c1aaaaa432903cfc050788592d25
Rudolf Martincsek <67291494+Rudiksz@users.noreply.github.com>
2021-06-11 13:40:00 +0300

    v0.2.2 - Export tab.dart too

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,6 @@
+## 0.2.2
+* Export tab.dart too
+
 ## 0.2.1
 * Fixed bug in splitting panels and null pointer exception caused by flutter
 
--- lib/tabpanel.dart
+++ lib/tabpanel.dart
@@ -3,6 +3,7 @@ library tabpanel;
 import 'package:uuid/uuid.dart';
 
 export 'src/tab_panel.dart';
+export 'src/tab.dart';
 export 'src/tab_panel_widget.dart';
 export 'src/tab_panel_theme.dart';
 
